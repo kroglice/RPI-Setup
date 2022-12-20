37d0f823710e6df0e1cbf3625bbf7de499c66328
Nuno Pereira <kroglice@users.noreply.github.com>
2022-12-20 10:32:36 +0000

    Update it works

--- usb.sh
+++ usb.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo apt update
-
-#sudo echo "dtoverlay=dwc2" >> /boot/config.txt
+echo 'Starting'
 echo 'dtoverlay=dwc2' | sudo tee --append /boot/config.txt
 sudo sed -i 's/rootwait/rootwait modules-load=dwc2,g_ether/' /boot/cmdline.txt
+echo 'Complete'
